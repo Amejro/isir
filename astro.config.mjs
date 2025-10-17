14cba4cbdad3dfbf91184875068c5b5f967d0a50
amejro <hansonnewton@gmail.com>
2025-10-17 19:37:15 +0000

    Metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Amejro.github.io',
+  base: '/isir',
   integrations: [vue()],
   prefetch: {
     defaultStrategy: 'viewport'
